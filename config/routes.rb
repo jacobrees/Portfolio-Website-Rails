fd9928c6f0bb13206da132987a3a4707f6a3e688
jacobrees <jacob.rees97@yahoo.com>
2023-04-11 08:01:09 +0100

    Add a resume button to about me page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/home'
   get '/about', to: 'pages#about'
+  get '/projects', to: 'pages#projects'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
